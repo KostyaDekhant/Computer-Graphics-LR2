3aa04c9a7459d85410795052bfa75ce78849de9e
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2024-11-24 23:25:33 +0700

    little changes

---
+++ Computer Graphics LR2/Point3D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Graphics_LR2
+{
+    public class Point3D
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public Point3D(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
